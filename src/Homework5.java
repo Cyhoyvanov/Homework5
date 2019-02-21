17861db2050833508f2800e6bb070032df5e3de3
unknown <cyhoyvanov@gmail.com>
2019-02-21 14:04:26 +0200

    first commit

---
+++ src/Homework5.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Homework5 {
+		public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Введите ваш вес:");
+	        System.out.println("Ваш вес на Луне равен: " + scanner.nextDouble()*0.17);
+	    }
+	}
